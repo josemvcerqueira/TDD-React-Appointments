804ed1fe435a17b45e67da73b727a3becd84dc98
Jose Cerqueira <jose@origensmedia.com>
2019-12-07 03:45:00 +0000

    added Appointment test to render the customer firstname

---
+++ test/Appointment.test.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Appointment } from '../src/Appointment';
+
+describe('Appointment', () => {
+  it('renders the customer first name', () => {
+    const customer = { firstName: 'Ashley' };
+    const container = document.createElement('div');
+    ReactDOM.render(<Appointment customer={customer} />, container);
+
+    expect(container.textContent).toMatch('Ashley');
+  });
+
+  it('renders the customer first name', () => {
+    const customer = { firstName: 'Jordan' };
+    const container = document.createElement('div');
+    ReactDOM.render(<Appointment customer={customer} />, container);
+
+    expect(container.textContent).toMatch('Jordan');
+  });
+});
